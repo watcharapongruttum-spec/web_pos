089332da65bab6aeef9e815c05be71ca5b2b56ce
mikkee888 <watcharapongruttum@gmail.com>
2025-12-26 09:29:31 +0700

    Add auth guard and protect dashboard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SkuMastersComponent } from './pages/sku-masters/sku-masters.component';
 import { UsersComponent } from './pages/users/users.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
 
-
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [authGuard],
+    canActivateChild: [authGuard],  
     children: [
       { path: 'sku-masters', component: SkuMastersComponent },
       { path: 'users', component: UsersComponent },
@@ -20,10 +22,10 @@ const routes: Routes = [
     ]
   },
 
-
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
